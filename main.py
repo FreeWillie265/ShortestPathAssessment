7544c9fc3ce93ffc8e8dc347f0b90437ed967568
FreeWillie265 <wkasakula@gmail.com>
2023-10-30 22:36:01 +0200

    Fix: add vertices and edges to the graph then write main execution to print the take user input before executing

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from graph import Graph
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def add_vertices(graph: Graph):
+    graph.add_vertex("Mchinji")     # 0
+    graph.add_vertex("Kasungu")     # 1
+    graph.add_vertex("Ntchisi")     # 2
+    graph.add_vertex("Nkhotakota")  # 3
+    graph.add_vertex("Dowa")        # 4
+    graph.add_vertex("Salima")      # 5
+    graph.add_vertex("Lilongwe")    # 6
+    graph.add_vertex("Dedza")       # 7
+    graph.add_vertex("Ntcheu")      # 8
 
 
-# Press the green button in the gutter to run the script.
+def add_edges(graph: Graph):
+    # Mchinji
+    graph.add_edge(0, 1, 1)
+    graph.add_edge(0, 6, 1)
+
+    # Kasungu
+    graph.add_edge(1, 2, 1)
+    graph.add_edge(1, 4, 1)
+
+    # Ntchisi
+    graph.add_edge(2, 4, 1)
+    graph.add_edge(2, 3, 1)
+
+    # Nkhotakota
+    graph.add_edge(3, 5, 1)
+
+    # Dowa
+    graph.add_edge(4, 5, 1)
+    graph.add_edge(4, 6, 1)
+
+    # Salima
+    graph.add_edge(5, 7, 1)
+
+    # Lilongwe
+    graph.add_edge(6, 7, 1)
+
+    # Dedza
+    graph.add_edge(7, 8, 1)
+
+    # Ntcheu's edge has already been defined at Dedza
+
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    exit_program = '1'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    graph = Graph()
+    add_vertices(graph)
+    add_edges(graph)
+
+    while exit_program == '1':
+        print("Choose a district from the following: \n"
+              "Mchinji, Lilongwe, Kasungu, Dowa, Ntchisi \n"
+              "Dedza, Ntcheu, Nkhotakota, Salima")
+        source = input("Enter the source district: ")
+        destination = input("Enter the destination district: ")
+
+        graph.execute(source, destination)
+
+        exit_program = input("Re-Try? \n "
+                             "1 for yes, any other character to exit")
