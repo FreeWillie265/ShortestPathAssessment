d962e7c82f645f7a58bccc4b925544d80176e3fa
FreeWillie265 <wkasakula@gmail.com>
2023-10-30 19:49:53 +0200

    Feature: Add method to check if two vertices are adjacent

--- graph.py
+++ graph.py
@@ -12,3 +12,12 @@ class Graph:
 
     def add_edge(self, edge: Edge):
         self.edges.append(edge)
+
+    def is_adjacent(self, vertex_1: int, vertex_2: int):
+        """Check if two vertices are adjacent to each other.
+        i.e. You can find an edge where the source is vertex 1
+         and the destination is vertex two or vice versa"""
+        for edge in self.edges:
+            if (edge.source_vertex == self.vertices[vertex_1] and edge.destination_vertex == self.vertices[vertex_2]) or (edge.source_vertex == self.vertices[vertex_2] and edge.destination_vertex == self.vertices[vertex_1]):
+                return True
+        return False
