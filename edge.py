98d6230eab80756918c82f9728cbdbe785d062ad
FreeWillie265 <wkasakula@gmail.com>
2023-10-30 22:01:25 +0200

    Fix: strict typing source and destination to proper types

--- edge.py
+++ edge.py
@@ -1,6 +1,8 @@
+from vertex import Vertex
+
 
 class Edge:
-    def __init__(self, source: int, destination: int, cost: int):
+    def __init__(self, source: Vertex, destination: Vertex, cost: int):
         self.source_vertex = source
         self.destination_vertex = destination
         self.cost = cost
